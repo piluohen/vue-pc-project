daa9913b220251a89fea40b404e800bdea27c104
piluohen <1012106967@qq.com>
2018-07-05 21:09:32 +0800

    Added：增加图片预览组件、增加非无限循环模式

---
+++ src/components/preview/videoPreview.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  el-dialog.video-preview-dialog(:visible.sync="videoData.showVideo", title="预览视频", append-to-body, width="840px", top="4%")
+    iframe(v-if="videoData.showVideo", :src="videoData.url", frameborder=0)
+    span(slot="footer", class="dialog-footer")
+      el-button(type="primary", size="small", @click="videoData.showVideo = false") 关 闭
+</template>
+<script>
+  export default {
+    name: 'VideoPreview',
+    data () {
+      return {
+      }
+    },
+    props: {
+      videoData: {
+        type: Object
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+<style lang="stylus">
+  .video-preview-dialog
+    iframe
+      width 800px
+      height 450px
+      video
+        width 100%
+</style>
