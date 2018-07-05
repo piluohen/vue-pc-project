c3de1b454bd34d600bb741cbd041e27b9f186fa7
piluohen <1012106967@qq.com>
2018-07-05 21:06:13 +0800

    Added：增加图片预览组件、增加非无限循环模式

---
+++ src/components/preview/imgPreview.vue
@@ -0,0 +1,101 @@
+<template lang="pug">
+  el-dialog.show-img-dialog(:visible.sync="imgData.showImg", top="0%", fullscreen, append-to-body)
+    el-carousel(height="100%", :initial-index="imgIndex", ref="carousel", :autoplay="false", :arrow="'always'", 
+    :indicator-position="'none'", :class="{ 'hide-left': hideLeft, 'hide-right': hideRight }" @change="changeHandle")
+      el-carousel-item(v-for="item in imgData.urlList", :key="item.url")
+        img(:src="item.url")
+</template>
+<script>
+  export default {
+    name: 'ImgPreview',
+    data () {
+      return {
+        hideLeft: false,
+        hideRight: false
+      }
+    },
+    props: {
+      // 图片数据
+      imgData: {
+        type: Object
+      },
+      // 图片索引
+      imgIndex: {
+        type: Number,
+        default: 0
+      },
+      // 是否无限滚动, 默认无限滚动
+      isInfinite: {
+        type: Boolean,
+        default: true
+      }
+    },
+    mounted () {
+    },
+    watch: {
+      imgIndex (val) {
+        // 监听imgIndex的值，如果已初始化了carousel且carousel的activeIndex与imgIndex不相等才执行重新对carousel赋值活动索引
+        if (this.$refs['carousel'] && this.$refs['carousel'].activeIndex !== val) {
+          this.$refs.carousel.setActiveItem(val)
+        }
+      }
+    },
+    methods: {
+      // 切换图片
+      changeHandle (index) {
+        if (!this.isInfinite) {
+          this.showArrowMethod(index)
+        }
+      },
+      // 显示左右箭头方法
+      showArrowMethod (index) {
+        if (index === 0) {
+          this.hideLeft = true
+        } else {
+          this.hideLeft = false
+        }
+        if (index === this.imgData.urlList.length - 1) {
+          this.hideRight = true
+        } else {
+          this.hideRight = false
+        }
+      }
+    }
+  }
+</script>
+<style lang="stylus">
+  .show-img-dialog
+    .el-dialog
+      background transparent
+      .el-dialog__header
+        padding 0
+        height 0
+        .el-dialog__headerbtn
+          background-color: rgba(31,45,61,.11);
+          border-radius 50%
+          z-index 100
+          .el-dialog__close
+            font-size 50px
+            color #fff
+      .el-dialog__body
+        height 100%
+        .el-carousel
+          height 100%
+          .el-carousel__item
+            display flex
+            justify-content center
+            align-items center
+          .el-carousel__arrow
+            width 80px
+            height 80px
+            font-size 50px
+          img
+            max-width: 80%
+            max-height: 100%
+          &.hide-left
+            .el-carousel__arrow--left
+              display none
+          &.hide-right
+            .el-carousel__arrow--right
+              display none
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,14 +1,105 @@
 <template lang="pug">
   .home-page
-    span 首页
+    div 首页
+    .checkbox-area
+      el-checkbox-group(v-for="v in allList", :key="v.id", v-model="checkList", @change="checkChangeHandle")
+        el-checkbox(v-for="item in v.list" :key="item.label", :label="item.value") {{item.label}}
+    .img-list
+      .item(v-for="(item, index) in imgList", :label="item", @click="showImgPreview(index)")
+        img(:src="item")
+    el-button(type="success", @click="showImgPreview(0)") 查看
+    ImgPreview(:imgData="imgData", :imgIndex="imgIndex", :isInfinite="false")
 </template>
 
 <script>
+  import ImgPreview from 'components/preview/imgPreview'
   export default {
-    name: 'HomePage'
+    name: 'HomePage',
+    components: {
+      ImgPreview
+    },
+    data () {
+      return {
+        checkList: [],
+        imgData: {
+          showImg: false,
+          urlList: []
+        },
+        imgIndex: 0,
+        allList: [
+          {
+            id: 0,
+            list: [
+              {label: '页面a', value: 'a'},
+              {label: '页面b', value: 'b'},
+              {label: '页面c', value: 'c'},
+              {label: '页面d', value: 'd'}
+            ]
+          },
+          {
+            id: 1,
+            list: [
+              {label: '页面e', value: 'e'},
+              {label: '页面f', value: 'f'},
+              {label: '页面g', value: 'g'},
+              {label: '页面h', value: 'h'}
+            ]
+          },
+          {
+            id: 2,
+            list: [
+              {label: '页面i', value: 'i'},
+              {label: '页面j', value: 'j'},
+              {label: '页面k', value: 'k'},
+              {label: '页面l', value: 'l'}
+            ]
+          }
+        ],
+        imgList: [
+          // require('assets/images/001.jpg'),
+          // require('assets/images/002.jpg'),
+          // require('assets/images/003.jpg'),
+          // require('assets/images/004.jpg'),
+          require('assets/images/005.jpg')
+        ]
+      }
+    },
+    methods: {
+      checkChangeHandle () {
+        console.log(this.checkList)
+      },
+      showImgPreview (index) {
+        let urlList = this.imgList.map(i => {
+          return {url: i}
+        })
+        this.imgData = {
+          showImg: true,
+          urlList: urlList
+        }
+        this.imgIndex = index
+      }
+    }
   }
 </script>
 
 <style lang="stylus">
-
+  .checkbox-area
+    width 600px
+    border 1px solid #dedede
+    padding 20px
+    display flex
+    justify-content space-around
+    .el-checkbox-group
+      // display flex
+      .el-checkbox
+        display block
+        margin 5px 0 0 0
+  .img-list
+    .item
+      width 100px
+      height 100px
+      img
+        display inline-block
+        width 100%
+        height 100%
 </style>
